3c7659b46fc3ccb25a60bdbb5378fc73a0eea579
Michal Samsel <smichals1997@gmail.com>
2020-08-12 19:51:42 +0200

    Added custom messages on failed validation.

--- app/Controllers/User.php
+++ app/Controllers/User.php
@@ -14,14 +14,40 @@ class User extends Controller
 
     public function register()
     {
+        $ruleMessages=[
+            'first_name' => [
+                'min_length' => 'W polu Imie jest zbyt mało znaków (minimum 3 znaki).',
+                'max_length' => 'W polu Imie jest zbyt dużo znaków (maksymalnie 100 znaków).',
+            ],
+            'last_name' => [
+                'min_length' => 'W polu Nazwisko jest zbyt mało znaków (minimum 3 znaki).',
+                'max_length' => 'W polu Nazwisko jest zbyt dużo znaków (maksymalnie 100 znaków).',
+            ],
+            'email' => [
+                'valid_email' => 'To nie jest poprawny adres email.',
+                'max_length' => 'W polu Email jest zbyt dużo znaków (maksymalnie 100 znaków).',
+                'isEmailUsed' => 'Podany email jest już wykorzystany.',
+            ],
+            'password' => [
+                'min_length' => 'Podane hasło jest zbyt krótkie (minimum 6 znaków).',
+                'max_length' => 'Podane haslo jest zbyt długie (maksymalnie 16 znaków).',
+            ],
+            'password_validate' => [
+                'matches' => 'Podane hasła nie są identyczne.',
+            ],
+            'account_type_id' => [
+                'in_list' => 'Wybierz jeden z dwóch typów konta.',
+            ],
+        ];
+
         if ($this->request->getMethod() === 'post' && $this->validate([
-            'first_name' => 'alpha|min_length[3]|max_length[100]',
-            'last_name' => 'alpha_dash|min_length[3]|max_length[100]',
+            'first_name' => 'min_length[3]|max_length[100]',
+            'last_name' => 'min_length[3]|max_length[100]',
             'email' => 'valid_email|max_length[100]|isEmailUsed',
             'password' => 'min_length[6]|max_length[16]',
             'password_validate' => 'matches[password]',
             'account_type_id' => 'in_list[1,2]',
-        ])) {
+        ], $ruleMessages)) {
             $userModel = new UserModel();
             $userModel->createUser(
                 $this->request->getPost('account_type_id'),
@@ -32,7 +58,7 @@ class User extends Controller
             );
 
             echo view('Views/templates/header');
-            echo 'Success';
+            echo 'Rejestracja przebiegła pomyślnie.';
             echo view('Views/templates/footer');
         } else {
             $accountTypeModel = new AccountTypeModel();
